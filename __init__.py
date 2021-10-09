194c81d517f9d0c49d5df5ffe1df73392059fb05
Asha-hash-debug <80579662+Asha-hash-debug@users.noreply.github.com>
2021-10-09 12:04:14 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config['DEBUG']=False
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'lakshmikn7387@gmail.com'
+app.config['MAIL_DEFAULT_SENDER'] = 'lakshmikn7387@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Ashakn@1994'
+mail = Mail(app)
+
+from sdwan import routes
